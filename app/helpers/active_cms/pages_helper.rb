21ffcc9919d6d7a473133a85330f403c1fc2d8ad
Frank Cieslik <cieslik@geisters.de>
2012-03-26 17:20:57 +0200

    changes in helper and increase version-number

--- app/helpers/active_cms/pages_helper.rb
+++ app/helpers/active_cms/pages_helper.rb
@@ -1,14 +1,16 @@
 module ActiveCms
   module PagesHelper
 
-    def get_pages_list(slug)
+    def get_pages_list(slug, include_self = true, self_as_link = false)
       page = ActiveCms::Page.where(:slug =>slug.to_s).first
       return "No page for '#{slug.to_s}' found." if !page || !page.children || page.children.size == 0
       
       ret = "<ul>"
+      ret << "<li>"+((self_as_link)? "<a href=\"#{page.link}\">#{page.title}</a>+" : "#{page.title}") + "<ul>" if include_self
       page.children.each do |item|
         ret << "<li class=\"\"><a href=\"#{item.link}\">#{item.title}</a></li>"
       end
+      ret << "</ul></li>" if include_self
       ret << "</ul>"
       raw(ret)
     end
--- lib/active_cms/version.rb
+++ lib/active_cms/version.rb
@@ -1,3 +1,3 @@
 module ActiveCms
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
